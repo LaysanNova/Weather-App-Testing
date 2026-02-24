7376099b7ff86385de0b2340abf7d1420744acb7
Laysan Nova <laysansh@gmail.com>
2026-02-23 17:44:36 -0700

    code check dependencies

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,33 +1,3 @@
-import { chromium, expect } from "@playwright/test";
-import { BASE_URL } from "./config/env";
-
-// export default async function globalSetup() {
-//     const retries = 5;
-//     const retryDelay = 5000;
-//
-//     const browser = await chromium.launch({ headless: true });
-//     const page = await browser.newPage();
-//
-//     for (let i = 0; i < retries; i++) {
-//         try {
-//             console.log(`Attempt ${i + 1} to wake up site: ${BASE_URL}`);
-//             await page.goto(BASE_URL, { waitUntil: 'load', timeout: 60000 });
-//             await expect(page).toHaveTitle(/MyApp/i, { timeout: 10000 });
-//             console.log('✅ Site is awake!');
-//             break;
-//         } catch (e) {
-//             console.log(`⚠ Attempt ${i + 1} failed.`);
-//             if (i === retries - 1) {
-//                 await browser.close();
-//                 throw new Error('Site did not wake up after retries');
-//             }
-//             await new Promise(res => setTimeout(res, retryDelay));
-//         }
-//     }
-//
-//     await browser.close();
-// }
-
 import { request } from "@playwright/test";
 
 export default async function globalSetup() {
--- pages/LocationPage.ts
+++ pages/LocationPage.ts
@@ -4,10 +4,6 @@ import { Page, expect } from "@playwright/test";
 export class LocationPage {
   constructor(private page: Page) {}
 
-  // async verifyLoaded() {
-  //     await expect(this.page).toHaveURL(/location/);
-  // }
-
   async expectTitle(expected: RegExp) {
     await expect(this.page).toHaveTitle(expected);
   }

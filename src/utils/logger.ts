ae06948f6b9d5a98c8357321412d339405b51337
Laysan Nova <laysansh@gmail.com>
2026-02-25 14:24:12 -0700

    logger install pino

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,19 +3,17 @@ import pino from "pino";
 const isCI = process.env.CI === "true";
 
 export const logger = pino(
-  isCI
-    ? {
-        level: process.env.LOG_LEVEL || "info",
-      }
-    : {
-        level: process.env.LOG_LEVEL || "debug",
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: "HH:MM:ss",
-            ignore: "pid,hostname",
-          },
-        },
-      },
+    {
+        level: process.env.LOG_LEVEL || 'info',
+    },
+    process.env.CI === 'true'
+        ? pino.transport({
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'HH:MM:ss',
+                ignore: 'pid,hostname',
+            },
+        })
+        : undefined
 );

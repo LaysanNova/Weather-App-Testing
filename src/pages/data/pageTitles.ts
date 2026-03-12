8b4a2b2d8f45bf8ec112b3fd96d2388518c004ee
Laysan Nova <laysansh@gmail.com>
2026-03-11 22:20:48 -0600

    history checking

--- src/pages/data/pageTitles.ts
+++ src/pages/data/pageTitles.ts
@@ -1,7 +1,7 @@
 export const pageTitles = [
   { label: 'HackerNews Page', key: 'hackerNews', expectedTitle: 'Hacker News' },
   { label: 'New Page', key: 'new', expectedTitle: 'New Links | Hacker News' },
-  { label: 'Past Page', key: 'past', expectedTitle: '2026-03-11 | Hacker News' },
+  { label: 'Past Page', key: 'past', expectedTitle: '2026-03-11 front | Hacker News' },
   { label: 'Comments Page', key: 'comments', expectedTitle: 'New Comments | Hacker News' },
   // { label: 'Threads Page', key: 'threads', expectedTitle: 'Threads - Hacker News' },
   // { label: 'Welcome Page', key: 'welcome', expectedTitle: 'Welcome to Hacker News' },

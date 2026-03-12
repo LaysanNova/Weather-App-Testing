25e8a2ab349c2d1ad7dae196bb3ba137106e78cb
Laysan Nova <laysansh@gmail.com>
2026-03-11 19:11:30 -0600

    allure report and reorg

--- src/run/location.ts
+++
@@ -1,6 +0,0 @@
-import { test } from "../fixtures/pages.fixture";
-import { expect } from "@playwright/test";
-
-test("Location page title", async ({ locationPage }) => {
-  expect(await locationPage.getTitle()).toMatch(/Weather/);
-});

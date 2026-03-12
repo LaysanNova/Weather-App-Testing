5c60bacf8b4718941c61da5451c832a7e31b190e
Laysan Nova <laysansh@gmail.com>
2026-03-11 22:14:04 -0600

    title test

--- src/pages/data/pageTitles.ts
+++ src/pages/data/pageTitles.ts
@@ -3,7 +3,5 @@ export const pageTitles = [
   { label: 'New Page', key: 'new', expectedTitle: 'New Links | Hacker News' },
   { label: 'Past Page', key: 'past', expectedTitle: 'Past Links | Hacker News' },
   { label: 'Comments Page', key: 'comments', expectedTitle: 'New Comments | Hacker News' },
-  // { label: 'Threads Page', key: 'threads', expectedTitle: 'Threads - Hacker News' },
-  // { label: 'Welcome Page', key: 'welcome', expectedTitle: 'Welcome to Hacker News' },
 ];
 
